5f93491107689db50771724aac22336296876fb8
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2025-09-18 14:17:21 -0700

    set up basic things in auth, also finished DB model and created entire DB in DBeaver, now need to port DB through SQLAlchemy

--- backend/auth.py
+++ backend/auth.py
@@ -1,9 +1,18 @@
 from passlib.context import CryptContext
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from database import get_db
+import models
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+db_dependency = Annotated[Session, Depends(get_db)]
 
 def get_password_hash(password):
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def get_user_by_id(user_id: int, db: db_dependency):
+    return db.query(models.Accounts).filter(user_id=user_id).first()
